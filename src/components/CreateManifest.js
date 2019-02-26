8adc16633cb293f79ef89750d0997b5d9566d10a
Will Schulzetenberg <wschulzetenberg@wddco.com>
2019-02-26 14:33:03 -0600

    Add templates

--- src/components/CreateManifest.js
+++ src/components/CreateManifest.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { Button, ProgressCircle } from 'react-desktop/windows'
-var JSZip = window.require('jszip')
-var fs = window.require('fs')
+import { Button, TextInput } from 'react-desktop/windows'
 const { dialog, systemPreferences } = window.require('electron').remote
 
 export default class extends Component {
@@ -12,6 +10,15 @@ export default class extends Component {
   render() {
     return (
       <div>
+        <TextInput
+          ref="input"
+          theme={this.props.theme}
+          color={this.props.color}
+          width={300}
+          background={'white'}
+          label="Content Name"
+          placeholder="The name of the content"
+        />
         <Button
           push
           color={this.props.color}
@@ -22,9 +29,10 @@ export default class extends Component {
               },
               function(files) {
                 if (files !== undefined) {
-                  // var zip = new JSZip()
+                  const fs = window.require('fs')
                   fs.readFile(files[0], function(err, data) {
                     if (err) throw err
+                    const JSZip = window.require('jszip')
                     JSZip.loadAsync(data).then(function(zip) {
                       console.log(zip)
                     })
@@ -36,8 +44,6 @@ export default class extends Component {
         >
           Select Archive
         </Button>
-
-        <ul id="archive" />
       </div>
     )
   }
